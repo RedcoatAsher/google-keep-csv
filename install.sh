addf29bc2d35bf237c755b218c935250c5b68418
Asher Peruscini <asherperuscini@gmail.com>
2024-10-25 14:08:06 +0100

    init

---
+++ install.sh
@@ -0,0 +1,16 @@
+# install homebrew (https://brew.sh)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install python3
+brew install python
+brew install pip
+pip install pipenv 
+
+pipenv install
+pipenv shell
+
+
+# backups
+# sudo python3 -m pip install beautifulsoup4
+# sudo python3 -m pip install dateutils
+# sudo python3 -m pip install unicodecsv
