46e87fe62526013fa446d689a0c6e21340dcae0d
Asher Peruscini <asherperuscini@gmail.com>
2024-10-25 13:46:21 +0100

    ^ requirements.txt + install.sh + uninstall.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo python3 -m pip install beautifulsoup4
+sudo python3 -m pip install dateutils
+sudo python3 -m pip install unicodecsv
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+sudo python3 -m pip uninstall -y beautifulsoup4
+sudo python3 -m pip uninstall -y dateutils
+sudo python3 -m pip uninstall -y unicodecsv
